8615761f1d15782c72bce12329cfb4acaf515ea4
yifrankliu <frank.liu@yale.edu>
2026-06-08 11:31:17 +0800

    implement unified_ppo_agent training file

--- sustain-lc/train_unified_mlp.py
+++ sustain-lc/train_unified_mlp.py
@@ -3,29 +3,22 @@ from datetime import datetime
 import torch
 import numpy as np
 from torch.utils.tensorboard import SummaryWriter
-from multiagent_ca_ppo import multiagent_ppo_dtde
+from unified_mlp_baseline import Unified_PPO
 from frontier_env import SmallFrontierModel
 
-# time to change
-def batchify_observations(observation_dict):
+def flatten_state(state_dict):
     """
-    This function takes a dictionary of observations and returns a batch of observations.
+    This function is modified, instead of batchifying for each component, it groups all
+    component observations into one array to be acted on by one unified agent.
     """
-    # Initialize an empty list to store the batch of observations
-    batch_dict = {'CDUCAB': [], 'CT': []}
-
-    # Iterate through the dictionary and append each observation to the batch
-    for key, value in observation_dict.items():
-        if key!= 'cooling-tower-1':
-            batch_dict['CDUCAB'].append(value)
-        else:
-            batch_dict['CT'].append(value)
-
-    batch_dict['CDUCAB'] = np.array(batch_dict['CDUCAB'])
-    batch_dict['CT'] = np.array(batch_dict['CT'])
+    flattened = np.concatenate([state_dict['cdu-cabinet-1'],
+                    state_dict['cdu-cabinet-2'],
+                    state_dict['cdu-cabinet-3'],
+                    state_dict['cdu-cabinet-4'],
+                    state_dict['cdu-cabinet-5'],
+                    state_dict['cooling-tower-1']])
     
-    # Convert the list to a numpy array
-    return batch_dict
+    return flattened
 
 def categorize_actions(actions):
     
@@ -55,8 +48,7 @@ def train():
     print_freq = max_ep_len * 10        # print avg reward in the interval (in num timesteps)
     log_freq = max_ep_len * 5           # log avg reward in the interval (in num timesteps)
     save_model_freq = int(2e3)          # save model frequency (in num timesteps)
-    best_reward = {'CDUCAB' : float('-inf'),
-                   'CT' : float('-inf')}  # initialize best reward as negative infinity
+    best_reward = float('-inf')  # initialize best reward as negative infinity
     print_avg_reward = 0                # initialize average reward
 
     action_std = 0.6                    # starting std for action distribution (Multivariate Normal)
@@ -74,7 +66,8 @@ def train():
     eps_clip = 0.2          # clip parameter for PPO
     gamma = 0.80            # discount factor
 
-    lr_actor = 0.0003       # learning rate for actor network
+    lr_cdu_actor = 0.0003
+    lr_ct_actor = 0.0003       # learning rate for actor network
     lr_critic = 0.001       # learning rate for critic network
 
     random_seed = 123         # set random seed if required (0 = no random seed)
@@ -86,22 +79,11 @@ def train():
     exogen_gen_v = 2
     env = SmallFrontierModel(use_reward_shaping = f'reward_shaping_v{reward_fn_version}', exogen_gen_v = exogen_gen_v)
     print("Using Reward Shaping Version : ", env.use_reward_shaping)
-    # placeholder agent_mdp_dict    
-    agent_mdp_dict = {'CDUCAB' : {'state_dim': env.observation_space['cdu-cabinet-1'].shape[0], 'action_dim': env.action_space['cdu-cabinet-1'].shape[0], 
-                                  'num_centralized_actions' : 5,
-                                    'lr_actor': lr_actor, 'lr_critic': lr_critic, 'gamma': gamma, 'K_epochs': K_epochs, 'eps_clip': eps_clip, 
-                                    'has_continuous_action_space': True, 'action_std_init': action_std},
-                      
-                     'CT' : {'state_dim': env.observation_space['cooling-tower-1'].shape[0], 'action_dim': env.action_space['cooling-tower-1'].n, 
-                                        'num_centralized_actions' : 1,
-                                        'lr_actor': lr_actor, 'lr_critic': lr_critic, 'gamma': gamma, 'K_epochs': K_epochs, 'eps_clip': eps_clip, 
-                                        'has_continuous_action_space': False, 'action_std_init': action_std}
-    }
 
     ###################### logging ######################
 
     #### log files for multiple runs are NOT overwritten
-    log_dir = "MA_CA_PPO_logs"
+    log_dir = "Unified_MLP_logs"
     if not os.path.exists(log_dir):
           os.makedirs(log_dir)
 
@@ -115,14 +97,14 @@ def train():
     run_num = len(current_num_files)
 
     #### create new log file for each run
-    log_f_name = log_dir + '/MA_CA_PPO_' + env_name + "_log_" + str(run_num) + ".csv"
+    log_f_name = log_dir + '/Unified_MLP_' + env_name + "_log_" + str(run_num) + ".csv"
 
     print("current logging run number for " + env_name + " : ", run_num)
     print("logging at : " + log_f_name)
     #####################################################
 
     ################### checkpointing ###################
-    directory = "MA_CA_PPO_preTrained"
+    directory = "Unified_MLP_preTrained"
     if not os.path.exists(directory):
           os.makedirs(directory)
 
@@ -130,50 +112,43 @@ def train():
     if not os.path.exists(directory):
           os.makedirs(directory)
 
-    # create checkpoint_path for each multi agent in a for loop. Assume there is a dictionary with list of agents called agent_mdp_dict
-    checkpoint_path = {}
-    for agent_id in agent_mdp_dict.keys():
-        checkpoint_path[agent_id] = directory + "PPO_{}_{}_{}_agent_{}.pth".format(env_name, random_seed, run_num_pretrained, agent_id)
-        print("save checkpoint path : " + checkpoint_path[agent_id])
+    # create one single checkpoint
+    checkpoint_path = directory + "PPO_{}_{}_{}_unified.pth".format(env_name, random_seed, run_num_pretrained)
     #####################################################
 
 
     ############# print all hyperparameters #############
-    for agent_id, agent_info in agent_mdp_dict.items():
+    print("--------------------------------------------------------------------------------------------")
+    print("Unified single-agent MLP (CDU continuous + CT discrete)")
+    print("max training timesteps : ", max_training_timesteps)
+    print("max timesteps per episode : ", max_ep_len)
+    print("model saving frequency : " + str(save_model_freq) + " timesteps")
+    print("log frequency : " + str(log_freq) + " timesteps")
+    print("printing average reward over episodes in last : " + str(print_freq) + " timesteps")
+    print("--------------------------------------------------------------------------------------------")
+    print("state space dimension : ", 34)
+    print("CDU action space dimension : ", 25)
+    print("CT action space dimension : ", 9)
+    print("--------------------------------------------------------------------------------------------")
+    print("starting std of CDU action distribution : ", action_std)
+    print("decay rate of std of action distribution : ", action_std_decay_rate)
+    print("minimum std of action distribution : ", min_action_std)
+    print("decay frequency of std of action distribution : " + str(action_std_decay_freq) + " timesteps")
+    print("--------------------------------------------------------------------------------------------")
+    print("PPO update frequency : " + str(update_timestep) + " timesteps")
+    print("PPO K epochs : ", K_epochs)
+    print("PPO epsilon clip : ", eps_clip)
+    print("discount factor (gamma) : ", gamma)
+    print("--------------------------------------------------------------------------------------------")
+    print("optimizer learning rate CDU actor : ", lr_cdu_actor)
+    print("optimizer learning rate CT actor : ", lr_ct_actor)
+    print("optimizer learning rate critic : ", lr_critic)
+    if random_seed:
         print("--------------------------------------------------------------------------------------------")
-        print("Agent:", agent_id)
-        print("max training timesteps : ", max_training_timesteps)
-        print("max timesteps per episode : ", max_ep_len)
-        print("model saving frequency : " + str(save_model_freq) + " timesteps")
-        print("log frequency : " + str(log_freq) + " timesteps")
-        print("printing average reward over episodes in last : " + str(print_freq) + " timesteps")
-        print("--------------------------------------------------------------------------------------------")
-        print("state space dimension : ", agent_info['state_dim'])
-        print("action space dimension : ", agent_info['action_dim'])
-        print("--------------------------------------------------------------------------------------------")
-        if agent_info['has_continuous_action_space']:
-            print("Initializing a continuous action space policy")
-            print("--------------------------------------------------------------------------------------------")
-            print("starting std of action distribution : ", agent_info['action_std_init'])
-            print("decay rate of std of action distribution : ", action_std_decay_rate)
-            print("minimum std of action distribution : ", min_action_std)
-            print("decay frequency of std of action distribution : " + str(action_std_decay_freq) + " timesteps")
-        else:
-            print("Initializing a discrete action space policy")
-        print("--------------------------------------------------------------------------------------------")
-        print("PPO update frequency : " + str(update_timestep) + " timesteps")
-        print("PPO K epochs : ", agent_info['K_epochs'])
-        print("PPO epsilon clip : ", agent_info['eps_clip'])
-        print("discount factor (gamma) : ", agent_info['gamma'])
-        print("--------------------------------------------------------------------------------------------")
-        print("optimizer learning rate actor : ", agent_info['lr_actor'])
-        print("optimizer learning rate critic : ", agent_info['lr_critic'])
-        if random_seed:
-            print("--------------------------------------------------------------------------------------------")
-            print("setting random seed to ", random_seed)
-        torch.manual_seed(random_seed)
-        env.seed(random_seed)  # pylint: disable=no-member
-        np.random.seed(random_seed)
+        print("setting random seed to ", random_seed)
+    torch.manual_seed(random_seed)
+    env.seed(random_seed)  # pylint: disable=no-member
+    np.random.seed(random_seed)
     #####################################################
 
     print("============================================================================================")
@@ -181,7 +156,10 @@ def train():
     ################# training procedure ################
 
     # initialize a PPO agent
-    ppo_agent = multiagent_ppo_dtde(agent_mdp_dict)
+    ppo_agent = Unified_PPO(state_dim=34, cdu_action_dim=25, ct_action_dim=9, num_centralized_actions=1,
+                            lr_cdu_actor=lr_cdu_actor, lr_ct_actor=lr_ct_actor, lr_critic=lr_critic,
+                            gamma=gamma, K_epochs=K_epochs, eps_clip=eps_clip,
+                            cdu_action_std_init=action_std)
 
     # track total training time
     start_time = datetime.now().replace(microsecond=0)
@@ -214,7 +192,7 @@ def train():
     while time_step <= max_training_timesteps:
 
         state = env.reset()
-        batch_state_dict = batchify_observations(state)
+        flattened = flatten_state(state)
         current_ep_reward = {'CDUCAB' : 0, 'CT' : 0}
         # create custom tb logging for cabinet episode reward
         cabinet_totalepisode_reward = {'cdu-cabinet-1':0, 'cdu-cabinet-2':0, 'cdu-cabinet-3':0, 'cdu-cabinet-4':0, 'cdu-cabinet-5':0}
@@ -223,35 +201,33 @@ def train():
 
             actions = {}  # list to store actions for each agent
 
-            # select action for CDU setups
-            action = ppo_agent.select_action(batch_state_dict['CDUCAB'], 'CDUCAB')
-            actions['CDUCAB'] = action
-            # select action for cooling tower
-            action = ppo_agent.select_action(batch_state_dict['CT'], 'CT')
-            actions['CT'] = action
+            # modified: one call for both cdu & ct actions
+            cdu_action, ct_action = ppo_agent.select_action(flattened)
+            actions['CDUCAB'] = cdu_action
+            actions['CT'] = ct_action
             
             # categorize actions in to dict format for env
             categorized_actions = categorize_actions(actions)
 
             state, rewards, done, _ = env.step(categorized_actions)
-            batch_state_dict = batchify_observations(state)
+            flattened = flatten_state(state)
             
-            # collect next state information for each agent; needed for calculating advantage to track the last state
-            # since this is a non terminating environment
-            ppo_agent.agents['CDUCAB'].next_state = batch_state_dict['CDUCAB']
-            ppo_agent.agents['CT'].next_state = batch_state_dict['CT']
-            
-            # collect reward and is_terminals for the cducab agent
-            ppo_agent.collect_rewards_and_terminals(rewards, done)
+            # collect next state for bootstrap value calculation
+            ppo_agent.next_state = flattened
 
+            # manually append rewards and terminals to each buffer
             reward = {
                     'CDUCAB' : (rewards['cdu-cabinet-1']+
                                 rewards['cdu-cabinet-2']+
                                 rewards['cdu-cabinet-3']+
                                 rewards['cdu-cabinet-4']+
-                                rewards['cdu-cabinet-5'])/5.0, 
+                                rewards['cdu-cabinet-5'])/5.0,
                         'CT' : rewards['cooling-tower-1']
                     }
+            ppo_agent.buffer_dict['cdu_action'].rewards.append(reward['CDUCAB'])
+            ppo_agent.buffer_dict['cdu_action'].is_terminals.append(done['cdu-cabinet-1'])
+            ppo_agent.buffer_dict['ct_action'].rewards.append(reward['CT'])
+            ppo_agent.buffer_dict['ct_action'].is_terminals.append(done['cooling-tower-1'])
             current_ep_reward['CDUCAB'] += reward['CDUCAB']
             current_ep_reward['CT'] += reward['CT']
             
@@ -264,16 +240,12 @@ def train():
             # update PPO agent for each agent
             if time_step % update_timestep == 0:
 
-                CDUCAB_ppo_loss = ppo_agent.update('CDUCAB')
-                writer.add_scalar('Loss_CDUCAB', CDUCAB_ppo_loss, time_step)
-                CT_ppo_loss = ppo_agent.update('CT')
-                writer.add_scalar('Loss_CT', CT_ppo_loss, time_step)
+                ppo_loss = ppo_agent.update()
+                writer.add_scalar('Loss', ppo_loss, time_step)
 
-            # if continuous action space; then decay action std of ouput action distribution for each agent
             if time_step % action_std_decay_freq == 0:
-                for agent_id in agent_mdp_dict.keys():
-                    if agent_mdp_dict[agent_id]['has_continuous_action_space']:
-                        ppo_agent.decay_action_std(action_std_decay_rate, min_action_std, agent_id)
+                ppo_agent.decay_action_std(action_std_decay_rate, min_action_std)
+    
 
             # log in logging file
             if time_step % log_freq == 0:
@@ -307,16 +279,14 @@ def train():
                 print_running_reward = {'CDUCAB' : 0, 'CT' : 0}
                 print_running_episodes = 0
 
-            # save model weights for each agent
+            # save loop removed, replaced with just one agent save
             if (time_step % save_model_freq  == 0):
-                for agent_id in agent_mdp_dict.keys():
-                    
-                    if (print_avg_reward[agent_id] > best_reward[agent_id]):
-                        best_reward[agent_id] = print_avg_reward[agent_id]
+                if (print_avg_reward['CDUCAB'] > best_reward):
+                        best_reward = print_avg_reward['CDUCAB']
                         print("--------------------------------------------------------------------------------------------")
-                        print(f"saving model for agent {agent_id} at : " + checkpoint_path[agent_id])
-                        ppo_agent.save(checkpoint_path[agent_id], agent_id)
-                        print(f"{agent_id} model saved")
+                        print("saving model at : " + checkpoint_path)
+                        ppo_agent.save(checkpoint_path)
+                        print("model saved")
 
                 print("Elapsed Time  : ", datetime.now().replace(microsecond=0) - start_time)
                 print("--------------------------------------------------------------------------------------------")
